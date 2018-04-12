c0679be6f818ce615d431f1c3ef8babfb9423594
Thanh Thanh <thanhtruong1216@gmail.com>
2018-04-13 03:15:58 +0700

    Update alt attribute of img tag

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'restaurant-picture-info';
+  image.alt = `${restaurant.cuisine_type} food restaurant`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
