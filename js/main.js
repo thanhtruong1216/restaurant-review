55cd6a60e4d03df1a578da6d64998ef9f5e90057
Thanh Thanh <thanhtruong1216@gmail.com>
2018-04-12 23:51:22 +0700

    Change h1 tag to h2 tag

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'restaurant-picture'
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
