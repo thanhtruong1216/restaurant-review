a337dc1cfb7cdf3c39d27bd1710c93ff50c95e43
Thanh Thanh <thanhtruong1216@gmail.com>
2018-04-13 03:19:44 +0700

    Update alt attribute of img tag

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `${restaurant.name} restaurant`;
+  image.alt = `${restaurant.name} restaurant in ${restaurant.neighborhood}`;
   li.append(image);
 
   const name = document.createElement('h2');
